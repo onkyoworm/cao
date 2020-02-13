a1c818dabefae7080f61463a6c445bfd39f025cd
Choohan <996570982@qq.com>
2020-02-13 15:27:59 +0800

    Update main.go
    rrrr

--- main.go
+++ main.go
@@ -44,6 +44,8 @@ func getBlock(stub shim.ChaincodeStubInterface, key string) (*Block, error) {
 //save a block on the ledger
 func putBlock(stub shim.ChaincodeStubInterface, key string, block *Block) error {
 	block_marshalled, _ := json.Marshal(*block)
+	iterator, _ := stub.GetHistoryForKey("key")
+	data, _ := iterator.Next()
 	return stub.PutState(key, block_marshalled)
 }
 
